e169bab39c8b96b686209779e43dd24acc8475af
sergiu.savin <sergiu.savin@ipsos.com>
2024-10-15 20:51:35 +0300

    Adaugare curs 12

--- lectia-11/practica/script.js
+++ lectia-11/practica/script.js
@@ -11,6 +11,37 @@ const technologies = ["Javascript", "Python", "CSS", "HTML"];
 6. Place ul tag with items on the page
 */
 
+const technologies = ["Javascript", "Python", "CSS", "HTML"];
+const containerEl = document.querySelector("#container");
+
+// console.dir(containerEl);
+
+const listEl = document.createElement("ul"); // <ul></ul>
+console.dir(listEl);
+containerEl.append(listEl);
+
+// const firstElemFromList = document.createElement('li')
+// firstElemFromList.innerText = technologies[0];
+// listEl.append(firstElemFromList)
+
+// for(let i = 0; i < technologies.length; i++) {
+//     const li = document.createElement('li');
+//     li.innerText = technologies[i]
+//     listEl.append(li);
+// }
+
+// technologies.sort();
+// technologies.forEach( (technology) => {
+//     const li = document.createElement('li');
+//     li.innerText = technology;
+//     listEl.prepend(li);
+// })
+
+technologies.sort().forEach((technology) => {
+  const li = document.createElement("li");
+  li.innerText = technology;
+  listEl.append(li);
+});
 
 /*
 # Task 2
@@ -23,8 +54,52 @@ Starting from this colors array, create buttons dynamically;
 4. Add the button to the UI
 */
 
+const colors = [
+  {
+    label: "Primary Button",
+    color: "#ff0000",
+    isPrimary: true,
+  },
+  {
+    label: "Click Me",
+    color: "#00ff00",
+    isPrimary: true,
+  },
+  {
+    label: "Yellow",
+    color: "#fff00f",
+    isPrimary: false,
+  },
+];
 
+colors.forEach((color) => {
+  const buttonEl = document.createElement("button"); // <button> </button>
+  containerEl.append(buttonEl);
+  buttonEl.textContent = color.label;
 
+  if (color.isPrimary) {
+    buttonEl.innerHTML = `<span><a href="https://www.google.ro/">${color.label}</a></span>`;
+    buttonEl.classList.add("primary-button");
+    // buttonEl.className = "primary-button test";
+    console.dir(buttonEl)
+  } else {
+    buttonEl.style.backgroundColor = color.color;
+  }
+});
 
+/**
+ * Test example
+ */
 
-
+const testQuerySelector = document.querySelector('#test');
+console.dir(testQuerySelector)
+// testQuerySelector.className = 'primary-button';
+testQuerySelector.classList.add('primary-button');
+const testQuerySelectorAll = document.querySelectorAll('#test')
+console.dir(testQuerySelectorAll[0])
+const testElementById = document.getElementById('test')
+console.dir(testElementById)
+const testElementByClassName = document.getElementsByClassName('test1')
+console.dir(testElementByClassName)
+const testElementByTagName = document.getElementsByTagName('div')
+console.dir(testElementByTagName)
