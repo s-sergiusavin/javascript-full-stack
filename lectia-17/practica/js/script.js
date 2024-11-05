19d90c2819c4fa1e53683fcca2ed2131a6658727
sergiu.savin <sergiu.savin@ipsos.com>
2024-11-05 18:21:54 +0200

    Lectii 17 & 18

---
+++ lectia-17/practica/js/script.js
@@ -0,0 +1,13 @@
+// Write a New Year's countdown timer that can be stopped by clicking the **Stop** button
+// Styles and markup can be taken here - https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_countdown
+
+/*
+New Year - January 1 00:00 2024
+Today's date is January 25, 20:20 2023
+
+we will use setInterval() to run the counter
+
+1. subtract the current date from the New Year - this way we will get the time difference (we will work with milliseconds)
+2. from the milliseconds obtained in step 1, we will extract the number of days, hours, minutes and seconds remaining until the New Year
+3. show this data to the user on the page
+*/
